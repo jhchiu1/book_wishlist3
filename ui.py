159f280df4a82ef3a34b19e413a6e6d52e11c5c9
lyellick-school <hk6127qf@go.minneapolis.edu>
2018-01-20 12:16:27 -0600

    added ui option: book search
    added method to ensure string is being passed

--- ui.py
+++ ui.py
@@ -2,7 +2,6 @@ from book import Book
 
 
 def display_menu_get_choice():
-
     '''Display choices for user, return users' selection'''
 
     print('''
@@ -10,6 +9,7 @@ def display_menu_get_choice():
         2. Show books that have been read
         3. Mark a book as read
         4. Add book to wishlist
+        5. Search for book
         q. Quit
     ''')
 
@@ -22,7 +22,7 @@ def show_list(books):
     ''' Format and display a list of book objects'''
 
     if len(books) == 0:
-        print ('* No books *')
+        print('* No books *')
         return
 
     for book in books:
@@ -32,7 +32,6 @@ def show_list(books):
 
 
 def ask_for_book_id():
-
     ''' Ask user for book id, validate to ensure it is a positive integer '''
 
     while True:
@@ -46,8 +45,18 @@ def ask_for_book_id():
             print('Please enter an integer number')
 
 
-def get_new_book_info():
+def ask_for_book_title():
+    ''' Ask user for book title, validate to ensure it is a string '''
+    while True:
+        search_string = input('Enter book title:')
+        if isinstance(search_string, str):
+            return search_string
+        else:
+            print('Please enter a string')
+            continue
 
+
+def get_new_book_info():
     ''' Get title and author of new book from user '''
 
     title = input('Enter title: ')
